65f722769bb995340da25ed3d2127943862a51a0
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-01-30 12:41:51 +1100

    commit tree 386d5b94f538bc8fb264593ead9f450c73b52439

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+const sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+
+function superbowlWin(records){
+    let winRecord = records.find((record) => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
